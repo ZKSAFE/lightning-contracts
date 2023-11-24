0dea1bb22dd8a107972d46b1d7b6191e52afc9c9
George <85392015@qq.com>
2023-11-24 10:49:36 +0800

    Update sellToken.test.js

--- test/sellToken.test.js
+++ test/sellToken.test.js
@@ -7,26 +7,27 @@ const IUniswapV3PoolABI = require('@uniswap/v3-core/artifacts/contracts/interfac
 
 //mainnet
 const ChainId = SupportedChainId.MAINNET
-const POOL_FACTORY_CONTRACT_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
-const QUOTER_CONTRACT_ADDRESS = '0x61fFE014bA17989E743c5F6cB21bF9697530B21e'
+const POOL_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+const QUOTER_ADDRESS = '0x61fFE014bA17989E743c5F6cB21bF9697530B21e'
 const SWAP_ROUTER_ADDRESS = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
-const WETH_CONTRACT_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-const USDC_CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-const USDT_CONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+const WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const USDT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+const DAI_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
 
 //localhost fork from mainnet
-const bundlerManagerAddr = '0x0B1a87021ec75fBaE919b1e86b2B1335FFC8F4d3'
-const bundlerAddr = '0x5434e8FC67e9285d0DA256f81359610faF34Bc02'
-const factoryAddr = '0x18eb8AF587dcd7E4F575040F6D800a6B5Cef6CAf'
-const walletAddr = '0x94f4C1743d0a8d4F1b7792DD34Cf5A9F5ea97BCD'
+// const bundlerManagerAddr = '0x0B1a87021ec75fBaE919b1e86b2B1335FFC8F4d3'
+// const bundlerAddr = '0x5434e8FC67e9285d0DA256f81359610faF34Bc02'
+// const factoryAddr = '0x18eb8AF587dcd7E4F575040F6D800a6B5Cef6CAf'
+// const walletAddr = '0x94f4C1743d0a8d4F1b7792DD34Cf5A9F5ea97BCD'
 
 const NATIVE_ETH = new Ether(ChainId)
-const WETH_TOKEN = new Token(ChainId, WETH_CONTRACT_ADDRESS, 18, 'WETH', 'Wrapped Ether')
-const USDC_TOKEN = new Token(ChainId, USDC_CONTRACT_ADDRESS, 6, 'USDC', 'USD Coin')
-const USDT_TOKEN = new Token(ChainId, USDT_CONTRACT_ADDRESS, 6, 'USDT', 'Tether USD')
+const WETH_TOKEN = new Token(ChainId, WETH_ADDRESS, 18, 'WETH', 'Wrapped Ether')
+const USDC_TOKEN = new Token(ChainId, USDC_ADDRESS, 6, 'USDC', 'USD Coin')
+const USDT_TOKEN = new Token(ChainId, USDT_ADDRESS, 6, 'USDT', 'Tether USD')
+const DAI_TOKEN = new Token(ChainId, DAI_ADDRESS, 18, 'DAI', 'Dai Stablecoin')
 
 const NATIVE_ETH_ADDRESS = '0x0000000000000000000000000000000000000000'
-
 const MAX_UINT256 = b('115792089237316195423570985008687907853269984665640564039457584007913129639935')
 
 
@@ -35,9 +36,12 @@ describe('sellToken.test', function () {
     let accounts
     let signer
     let provider
+    let usdc
+    let weth
     let wallet
     let bundler
     let bundlerManager
+    let quoterV3
 
     before(async function () {
         accounts = await ethers.getSigners()
@@ -47,65 +51,84 @@ describe('sellToken.test', function () {
 
         console.log('signer', signer.address)
         console.log('chainId', chainId)
+
+        const ERC20 = await ethers.getContractFactory('MockERC20', signer)
+        usdc = ERC20.attach(USDC_ADDRESS)
+        weth = ERC20.attach(WETH_ADDRESS)
     })
 
-    // it('deploy BundlerManager Bundler WalletFactory', async function () {
-    //     const BundlerManager = await ethers.getContractFactory('BundlerManager')
-    //     bundlerManager = await BundlerManager.deploy()
-    //     await bundlerManager.deployed()
-    //     console.log('const bundlerManagerAddr =', bundlerManager.address)
 
-    //     const Bundler = await ethers.getContractFactory('Bundler')
-    //     bundler = Bundler.attach(await bundlerManager.bundler())
-    //     console.log('const bundlerAddr =', bundler.address)
-
-    //     const WalletFactory = await ethers.getContractFactory('WalletFactory')
-    //     factory = await WalletFactory.deploy()
-    //     await factory.deployed()
-    //     console.log('const factoryAddr =', factory.address)
-    // })
-
-    // it('createWallet', async function () {
-    //     const SmartWallet = await ethers.getContractFactory('SmartWallet')
-    //     let tx = await (await factory.createWallet(accounts[1].address, bundler.address)).wait()
-    //     let walletAddr = tx.events[0].args[0]
-    //     wallet = SmartWallet.attach(walletAddr)
-    //     console.log('const walletAddr =', wallet.address)
-    // })
-
-
-    it('attach', async function () {
+    it('deploy BundlerManager Bundler WalletFactory QuoterV3', async function () {
         const BundlerManager = await ethers.getContractFactory('BundlerManager')
-        bundlerManager = BundlerManager.attach(bundlerManagerAddr)
+        bundlerManager = await BundlerManager.deploy()
+        await bundlerManager.deployed()
+        console.log('const bundlerManagerAddr =', bundlerManager.address)
 
         const Bundler = await ethers.getContractFactory('Bundler')
-        bundler = Bundler.attach(bundlerAddr)
+        bundler = Bundler.attach(await bundlerManager.bundler())
+        console.log('const bundlerAddr =', bundler.address)
 
         const WalletFactory = await ethers.getContractFactory('WalletFactory')
-        factory = WalletFactory.attach(factoryAddr)
+        factory = await WalletFactory.deploy([USDC_ADDRESS, USDT_ADDRESS, DAI_ADDRESS], 0)
+        await factory.deployed()
+        console.log('const factoryAddr =', factory.address)
+
+        const QuoterV3 = await ethers.getContractFactory('QuoterV3')
+        quoterV3 = await QuoterV3.deploy(POOL_FACTORY_ADDRESS, WETH_ADDRESS)
+        await quoterV3.deployed()
+        console.log('const quoterV3Addr =', quoterV3.address)
+    })
+
+
+    it('createWallet', async function () {
+        let nonce = await factory.nonceOf(accounts[0].address)
+        nonce++
+        let walletAddr = await factory.computeWalletAddr(accounts[0].address, nonce)
+    
+        await factory.createWallet(accounts[0].address, bundler.address)
+        
+        let hasWallet =  await factory.wallets(walletAddr)
+        console.log('wallet is created:', hasWallet)
 
         const SmartWallet = await ethers.getContractFactory('SmartWallet')
         wallet = SmartWallet.attach(walletAddr)
+        console.log('const walletAddr =', wallet.address)
 
         await print()
     })
 
 
-    // it('deposit', async function () {
-    //     await accounts[0].sendTransaction({to: wallet.address, value: m(10, 18)})
-    //     console.log('transfer ETH to', wallet.address)
+    // it('attach', async function () {
+    //     const BundlerManager = await ethers.getContractFactory('BundlerManager')
+    //     bundlerManager = BundlerManager.attach(bundlerManagerAddr)
 
-    //     // await accounts[0].sendTransaction({to: bundler.address, value: m(5, 18)})
-    //     // console.log('transfer ETH to', bundler.address)
+    //     const Bundler = await ethers.getContractFactory('Bundler')
+    //     bundler = Bundler.attach(bundlerAddr)
+
+    //     const WalletFactory = await ethers.getContractFactory('WalletFactory')
+    //     factory = WalletFactory.attach(factoryAddr)
+
+    //     const SmartWallet = await ethers.getContractFactory('SmartWallet')
+    //     wallet = SmartWallet.attach(walletAddr)
 
     //     await print()
     // })
-   
+
+
+    it('deposit', async function () {
+        await accounts[0].sendTransaction({to: wallet.address, value: m(10, 18)})
+        console.log('transfer ETH to', wallet.address)
+
+        // await accounts[0].sendTransaction({to: bundler.address, value: m(5, 18)})
+        // console.log('transfer ETH to', bundler.address)
+
+        await print()
+    })
    
 
     let tokenIn = NATIVE_ETH //NATIVE_ETH
-    let tokenOut = USDT_TOKEN
-    let amountIn = m(1, tokenIn.decimals)
+    let tokenOut = USDC_TOKEN
+    let amountIn = m(9, tokenIn.decimals)
     let amountOut
     let swapRoute
     it('quote', async function () {
@@ -132,7 +155,7 @@ describe('sellToken.test', function () {
         )
 
         let quoteCallReturnData = await provider.call({
-            to: QUOTER_CONTRACT_ADDRESS,
+            to: QUOTER_ADDRESS,
             data: calldata,
         })
 
@@ -140,9 +163,10 @@ describe('sellToken.test', function () {
         console.log('amountOut:', d(amountOut, tokenOut.decimals))
     })
 
+
     async function getPool(tokenA, tokenB, tradeFee) {
         let currentPoolAddress = computePoolAddress({
-            factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
+            factoryAddress: POOL_FACTORY_ADDRESS,
             tokenA: tokenA.isNative ? WETH_TOKEN : tokenA,
             tokenB: tokenB.isNative ? WETH_TOKEN : tokenB,
             fee: tradeFee,
@@ -211,7 +235,7 @@ describe('sellToken.test', function () {
 
         let options = {
             slippageTolerance: new Percent(1, 10000),
-            deadline: Math.floor(Date.now() / 1000) + 60,
+            deadline: Math.floor(Date.now() / 1000) + 600,
             recipient: bundler.address
         }
         let methodParameters = SwapRouter.swapCallParameters([uncheckedTrade], options)
@@ -225,11 +249,11 @@ describe('sellToken.test', function () {
         to = bundler.address
         value = 0
         let wantUSDAmount = amountOut.sub(m(2, tokenOut.decimals))
-        data = ERC20.interface.encodeFunctionData('transfer(address,uint256)', [wallet.address, wantUSDAmount])
+        data = ERC20.interface.encodeFunctionData('transfer(address,uint256)', [accounts[0].address, wantUSDAmount])
         data = Bundler.interface.encodeFunctionData('bundlerCallback(address,uint256,bytes)', [tokenOut.address, 0, data])
         callArr.push({to, value, data})
         
-        atomSignParams = await atomSign(accounts[1], wallet.address, callArr)
+        atomSignParams = await atomSign(accounts[0], wallet.address, callArr)
         console.log('atomSign done')
     })
 
@@ -251,13 +275,21 @@ describe('sellToken.test', function () {
 
 
     async function atomSign(signer, fromWallet, callArr) {
-        let atomCallBytes = convertCallArrToCallBytes(callArr)
+        let atomCallBytes = '0x'
+        for (let i=0; i<callArr.length; i++) {
+            let to = callArr[i].to
+            let value = callArr[i].value
+            let data = callArr[i].data
+            
+            let len = utils.arrayify(data).length
+            atomCallBytes = utils.hexConcat([atomCallBytes, to, utils.hexZeroPad(value, 32), utils.hexZeroPad(len, 32), data])
+        }
 
         let deadline = parseInt(Date.now() / 1000) + 600;
         let chainId = (await provider.getNetwork()).chainId
         let SmartWallet = await ethers.getContractFactory('SmartWallet')
-        let wallet = SmartWallet.attach(fromWallet)
-        let valid = await wallet.valid()
+        let hasWallet =  await factory.wallets(fromWallet)
+        let valid = hasWallet ? await SmartWallet.attach(fromWallet).valid() : 1
 
         let calldata = SmartWallet.interface.encodeFunctionData('atomSignCall', [atomCallBytes, deadline, '0x'])
         calldata = utils.hexConcat([calldata, utils.hexZeroPad(chainId, 31), fromWallet, utils.hexZeroPad(valid, 4)])
@@ -269,26 +301,12 @@ describe('sellToken.test', function () {
     }
 
 
-    function convertCallArrToCallBytes(callArr) {
-        let atomCallBytes = '0x'
-        for (let i=0; i<callArr.length; i++) {
-            let to = callArr[i].to
-            let value = callArr[i].value
-            let data = callArr[i].data
-            
-            let len = utils.arrayify(data).length
-            atomCallBytes = utils.hexConcat([atomCallBytes, to, utils.hexZeroPad(value, 32), utils.hexZeroPad(len, 32), data])
-        }
-        return atomCallBytes
-    }
-
-
     async function print() {
         console.log('')
 
         const ERC20 = await ethers.getContractFactory('MockERC20', signer)
         let usd = ERC20.attach(tokenOut.address)
-        let weth = ERC20.attach(WETH_CONTRACT_ADDRESS)
+        let weth = ERC20.attach(WETH_ADDRESS)
         
         console.log('account0 usd:', d(await usd.balanceOf(accounts[0].address), tokenOut.decimals), 'weth:', d(await weth.balanceOf(accounts[0].address), 18), 'eth:', d(await provider.getBalance(accounts[0].address), 18))
         console.log('account1 usd:', d(await usd.balanceOf(accounts[1].address), tokenOut.decimals), 'weth:', d(await weth.balanceOf(accounts[1].address), 18), 'eth:', d(await provider.getBalance(accounts[1].address), 18))
