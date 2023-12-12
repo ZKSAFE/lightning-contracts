968ee483e89037c70c7c07ba720e21d36f0c7733
George <85392015@qq.com>
2023-12-12 15:46:52 +0800

    update chainInfos

--- scripts/v2/chainInfos.js
+++ scripts/v2/chainInfos.js
@@ -13,64 +13,13 @@ module.exports = {
     //     SwapRouterAddr: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     //     WETHAddr: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     //     WETH_TOKEN: new Token(1, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
-
-    //     WalletFactoryAddr: '',
     //     USDAddrs: {
     //         USDCAddr: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     //         USDTAddr: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
     //         DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
     //     },
-    //     QuoterV3Addr: '',
-    //     BundlerManagerAddr: '',
-    //     BundlerAddr: '',
-    // },
-    // 5: {
-    //     rpc: {
-    //         name: "Ethereum Goerli",
-    //         url: "https://eth-goerli.g.alchemy.com/v2/demo",
-    //         chainId: 5
-    //     },
-    //     PoolFactoryAddr: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
-    //     QuoterAddr: '0x61fFE014bA17989E743c5F6cB21bF9697530B21e',
-    //     SwapRouterAddr: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    //     WETHAddr: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
-    //     WETH_TOKEN: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-
-    //     NATIVE_ETH_ADDRESS: '0x0000000000000000000000000000000000000000',
-    //     WalletFactoryAddr: '0xc3E408dDF2e03e11890C212D6D976b3fD6B87F9f',
-    //     USDAddrs: {
-    //         USDCAddr: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
-    //         DAI: '0x11fe4b6ae13d2a6055c8d9cf65c55bac32b5d844'
-    //     },
-    //     SubBundlerAddr: '0x8C1C64E6e5a5e31A9B6bd6728c2ad37838a0D301',
-    //     QuoterV3Addr: '0x42651ae9f9aae9ac51fd155dd4e98240e11e1344',
-    //     BundlerManagerAddr: '0x694dD96Ce948Fa6eE48BfA4B0e97B2aB96568B27',
     // },
 
-    // 31337: {
-    //     rpc: {
-    //         name: "Local Fork Ethereum Mainnet",
-    //         url: "http://localhost:8545",
-    //         chainId: 31337
-    //     },
-    //     PoolFactoryAddr: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
-    //     QuoterAddr: '0x61fFE014bA17989E743c5F6cB21bF9697530B21e',
-    //     SwapRouterAddr: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    //     WETHAddr: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    //     WETH_TOKEN: new Token(31337, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
-    //     WalletFactoryAddr: '',
-    //     USDAddrs: {
-    //         USDCAddr: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    //         USDTAddr: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-    //         DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-    //     },
-    //     QuoterV3Addr: '',
-    //     BundlerManagerAddr: '',
-    //     BundlerAddr: '',
-    //     PublicSocialRecoveryAddr: '',
-    //     AdminWalletAddr: '',
-    //     AdminEOAAddr: ''
-    // },
     137: {
         rpc: {
             name: "Polygon",
@@ -85,8 +34,7 @@ module.exports = {
         USDAddrs: {
             USDTAddr: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
             USDCAddr: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
-            DAI: '0x8F3cf7ad23cd3cADbd9735afe958023239c6a063',
-
+            DAIAddr: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
         },
         //2023-12-5
         QuoterV3Addr: '0x8f44d38611096E48E24391221C31615914792556',
@@ -99,30 +47,32 @@ module.exports = {
         AdminUUID: new ObjectId('656ebda11b118e6f59648f6b')
     },
 
-    // 56: {
-    //     rpc: {
-    //         name: "BNBChain",
-    //         url: "https://bsc.nodereal.io",
-    //         chainId: 56
-    //     },
-    //     PoolFactoryAddr: '0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7',
-    //     QuoterAddr: '0x78D78E420Da98ad378D7799bE8f4AF69033EB077',
-    //     SwapRouterAddr: '0x11AF00243EEdEae29B07E0e9A26e1CcE98df8c29',
-    //     WETHAddr: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    //     WETH_TOKEN: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
+    56: {
+        rpc: {
+            name: "BNBChain",
+            url: "https://bsc.nodereal.io",
+            chainId: 56
+        },
+        PoolFactoryAddr: '0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7',
+        QuoterAddr: '0x78D78E420Da98ad378D7799bE8f4AF69033EB077',
+        SwapRouterAddr: '0x11AF00243EEdEae29B07E0e9A26e1CcE98df8c29',
+        WETHAddr: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+        WETH_TOKEN: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
+        USDAddrs: {
+            USDTAddr: '0x55d398326f99059fF775485246999027B3197955',
+            USDCAddr: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+            BUSDAddr: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+        },
+        //2023-12-6
+        BundlerManagerAddr: '0x8f44d38611096E48E24391221C31615914792556',
+        WalletFactoryAddr: '0x0474d169D5d89f060D3e92861d787F4bE80A71dd',
+        QuoterV3Addr: '0x694dD96Ce948Fa6eE48BfA4B0e97B2aB96568B27',
+        MulticallAddr: '0x0554CE0BA18f6b2744973476838dB12FaE77bF94',
+        BundlerAddr: '0x7190544403c5EBd7Ba3882262f68CB8eE0E3c396',
+        AdminWalletAddr: '0x0E21854ca88BC3347dC87cFbFb6E4562d723D859',
+        AdminUUID: new ObjectId('656ebda11b118e6f59648f6b')
+    },
 
-    //     NATIVE_ETH_ADDRESS: '0x0000000000000000000000000000000000000000',
-    //     WalletFactoryAddr: '',
-    //     USDAddrs: {
-    //         USDCAddr: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
-    //         USDTAddr: '0x55d398326f99059fF775485246999027B3197955',
-    //         BUSDAddr: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-
-    //     },
-    //     SubBundlerAddr: '0xd6322a2842b0818505Fd810179AC401c461E1397',
-    //     QuoterV3Addr: '0x42651ae9f9aae9ac51fd155dd4e98240e11e1344',
-    //     BundlerManagerAddr: '0x694dD96Ce948Fa6eE48BfA4B0e97B2aB96568B27',
-    // },
     // 10: {
     //     rpc: {
     //         name: "Optimism",
@@ -134,16 +84,45 @@ module.exports = {
     //     SwapRouterAddr: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     //     WETHAddr: '0x4200000000000000000000000000000000000006',
     //     WETH_TOKEN: new Token(10, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
-
-    //     NATIVE_ETH_ADDRESS: '0x0000000000000000000000000000000000000000',
-    //     WalletFactoryAddr: '0xb58eD778643bEC922966334fea7a37834338FedE',
     //     USDAddrs: {
-    //         USDCAddr: '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
+    //         USDCAddr: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
     //         USDTAddr: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
     //         DAI: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1'
     //     },
-    //     SubBundlerAddr: '0x4B394eCf83dB82250dd5D988dF413A5a9092dd2e',
-    //     QuoterV3Addr: '0x42651ae9f9aae9ac51fd155dd4e98240e11e1344',
-    //     BundlerManagerAddr: '0x694dD96Ce948Fa6eE48BfA4B0e97B2aB96568B27',
+    // },
+
+    // 42161: {
+    //     rpc: {
+    //         name: "ArbitrumOne",
+    //         url: "https://arb1.arbitrum.io/rpc",
+    //         chainId: 42161
+    //     },
+    //     PoolFactoryAddr: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+    //     QuoterAddr: '0x61fFE014bA17989E743c5F6cB21bF9697530B21e',
+    //     SwapRouterAddr: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
+    //     WETHAddr: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    //     WETH_TOKEN: new Token(42161, '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 18, 'WETH', 'Wrapped Ether'),
+    //     USDAddrs: {
+    //         USDCAddr: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
+    //         USDTAddr: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
+    //         DAI: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1'
+    //     },
+    // },
+
+    // 8453: {
+    //     rpc: {
+    //         name: "Base",
+    //         url: "https://mainnet.base.org",
+    //         chainId: 8453
+    //     },
+    //     PoolFactoryAddr: '0x33128a8fC17869897dcE68Ed026d694621f6FDfD',
+    //     QuoterAddr: '0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a',
+    //     SwapRouterAddr: '0x2626664c2603336E57B271c5C0b26F421741e481',
+    //     WETHAddr: '0x4200000000000000000000000000000000000006',
+    //     WETH_TOKEN: new Token(8453, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
+    //     USDAddrs: {
+    //         USDCAddr: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    //         USDbCAddr: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA'
+    //     },
     // },
 }
--- scripts/v2/deploy.js
+++ scripts/v2/deploy.js
@@ -24,15 +24,26 @@ async function main() {
 
 	const needUSD = 1
 
-	//use the 1st USD Token
+	//pay $1 to activate AdminWallet
 	const ERC20 = await ethers.getContractFactory('MockERC20')
 	let usd = ERC20.attach(Object.values(chainInfo.USDAddrs)[0])
 	let usdBalance = await usd.balanceOf(signer.address)
 	let decimals = await usd.decimals()
+	console.log(await usd.symbol() + ' balance:', d(usdBalance, decimals))
 	if (usdBalance.lt(m(needUSD, decimals))) {
 		console.log("need $" + needUSD + ' of ' + Object.keys(chainInfo.USDAddrs)[0])
 		return
 	}
+
+	const BundlerManager = await ethers.getContractFactory('BundlerManager')
+	let bundlerManager = await BundlerManager.deploy()
+	await bundlerManager.deployed()
+	console.log('BundlerManagerAddr:', bundlerManager.address)
+
+	const WalletFactory = await ethers.getContractFactory('WalletFactory')
+	let factory = await WalletFactory.deploy(Object.values(chainInfo.USDAddrs), needUSD)
+	await factory.deployed()
+	console.log('WalletFactoryAddr:', factory.address)
 
 	const QuoterV3 = await ethers.getContractFactory('QuoterV3')
 	let quoterV3 = await QuoterV3.deploy(chainInfo.PoolFactoryAddr, chainInfo.WETHAddr)
@@ -44,30 +55,18 @@ async function main() {
 	await multicall.deployed()
 	console.log('MulticallAddr:', multicall.address)
 
-	const BundlerManager = await ethers.getContractFactory('BundlerManager')
-	let bundlerManager = await BundlerManager.deploy()
-	await bundlerManager.deployed()
-	console.log('BundlerManagerAddr:', bundlerManager.address)
-
 	const Bundler = await ethers.getContractFactory('Bundler')
 	let bundler = Bundler.attach(await bundlerManager.bundler())
 	console.log('BundlerAddr:', bundler.address)
 
-	const WalletFactory = await ethers.getContractFactory('WalletFactory')
-	let factory = await WalletFactory.deploy(Object.values(chainInfo.USDAddrs), needUSD)
-	await factory.deployed()
-	// let factory = WalletFactory.attach(chainInfo.WalletFactoryAddr)
-	console.log('WalletFactoryAddr:', factory.address)
-
-	let AdminUUID = new ObjectId()
-	console.log('AdminUUID:', AdminUUID)
-	let uuidBytes32 = uuidToBytes32(AdminUUID)
+	console.log('AdminUUID:', chainInfo.AdminUUID)
+	let uuidBytes32 = uuidToBytes32(chainInfo.AdminUUID)
 	let predictedAddr = await factory.computeWalletAddr(uuidBytes32)
 	await (await usd.transfer(predictedAddr, m(needUSD, decimals))).wait()
 	console.log('deposit $1 to predictedAddr:', predictedAddr)
 
 	let tx = await (await factory.createWallet(
-		uuidBytes32, signer.address, chainInfo.BundlerAddr)).wait()
+		uuidBytes32, signer.address, bundler.address)).wait()
 	let walletAddr
 	for (let event of tx.events) {
 		if (event.address == factory.address) {
